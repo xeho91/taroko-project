0529767e97b671bbf8c91dcb68de8cebafbef707
xeho91 <xeho91@pm.me>
2021-07-01 18:23:26 +0800

    build(vite): 🔧  Configure path aliases in `tsconfig.json`

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	assetsInclude: "./static",
-	plugins: [reactRefresh()],
+	plugins: [
+		reactRefresh(),
+		tsConfigPaths({
+			extensions: [
+				".js",
+				".jsx",
+				".ts",
+				".tsx",
+				"json",
+				".css",
+				".scss",
+			],
+		}),
+	],
 });
