5cff360a5666b039095fbd054710f191995ccc5d
xeho91 <xeho91@pm.me>
2021-07-05 06:33:26 +0800

    fix(view/edit routes): 🐛  Use correct id from the contact object

--- src/routes/edit.tsx
+++ src/routes/edit.tsx
@@ -16,43 +16,52 @@ interface EditRouteParams {
 
 type EditComponentProps = RouteComponentProps<EditRouteParams>;
 
+type EditableContactSchemaKey = keyof Omit<ContactSchema, "id">;
+// FIXME: Make it more strict.
+type EditableContactSchemaValue = string;
+
 const EditContact: FunctionComponent<EditComponentProps> = (props) => {
+    const history = useHistory();
     const { state: { list }, editContact } = useContext(ContactsContext);
     const contactId = parseInt(props.match.params.id);
-    const [contactData, setContactData] = useState(list[contactId - 1]);
-    const history = useHistory();
-
-    function handleSubmit(e: FormEvent) {
-        e.preventDefault();
-        editContact(contactData);
-        history.push("/");
-    }
-
-    type EditableContactSchemaKey = keyof Omit<ContactSchema, "id">;
-    // FIXME: Make it more strict.
-    type EditableContactSchemaValue = string;
-
-    const handleOnChange = (
-        key: EditableContactSchemaKey,
-        value: EditableContactSchemaValue,
-    ) => {
-        return setContactData({ ...contactData, [key]: value });
-    };
+    const [contactData, setContactData] = useState(
+        list.find(({ id }) => id === contactId),
+    );
 
     if (!contactData) {
         return <p>Invalid contact ID.</p>;
     } else {
+
+		const handleSubmit = (e: FormEvent) => {
+			e.preventDefault();
+			editContact(contactData);
+			history.push("/");
+		}
+
+        const handleOnChange = (
+            key: EditableContactSchemaKey,
+            value: EditableContactSchemaValue,
+        ) => {
+            return setContactData({ ...contactData, [key]: value });
+        };
+
         return (
             <Form
                 onSubmit={handleSubmit}
+
                 firstNameValue={contactData.first_name}
-                onFirstNameChange={(e) => handleOnChange("first_name", e.target.value)}
+                onFirstNameChange={(e) =>
+                    handleOnChange("first_name", e.target.value)}
+
                 lastNameValue={contactData.last_name}
                 onLastNameChange={(e) => handleOnChange("last_name", e.target.value)}
+
                 jobValue={contactData.job}
                 onJobChange={(e) => handleOnChange("job", e.target.value)}
+
                 descriptionValue={contactData.description}
-                onDescriptionChange={(e) => handleOnChange("description", e.target.value)}
+                onDescriptionChange={(e) =>
+                    handleOnChange("description", e.target.value)}
             />
         );
     }
--- src/routes/view.tsx
+++ src/routes/view.tsx
@@ -13,7 +13,7 @@ type ViewComponentProps = RouteComponentProps<ViewRouteParams>;
 const ViewContact: FunctionComponent<ViewComponentProps> = (props) => {
 	const contactId = parseInt(props.match.params.id);
     const { state: { list } } = useContext(ContactsContext);
-    const [contactData] = useState(list[contactId - 1]);
+    const [contactData] = useState(list.find(({ id }) => id === contactId));
 
     if (!contactData) {
         return (
