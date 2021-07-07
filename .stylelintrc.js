22df13a37cb92a25e58d5384e33792e38fb17496
xeho91 <xeho91@pm.me>
2021-07-07 19:19:52 +0800

    build(CSS): 💄  Improve styling & responsiveness

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-	extends: [
-		"stylelint-config-recommended",
-		"stylelint-a11y/recommended",
-		"stylelint-config-recess-order",
-		"stylelint-config-prettier",
-	],
+    extends: [
+        "stylelint-config-recommended",
+        "stylelint-a11y/recommended",
+        "stylelint-config-recess-order",
+        "stylelint-config-prettier",
+    ],
 
-	plugins: [
-		"stylelint-a11y",
-		"stylelint-scss",
-		"stylelint-no-unsupported-browser-features",
-	],
+    plugins: [
+        "stylelint-a11y",
+        "stylelint-scss",
+        "stylelint-no-unsupported-browser-features",
+    ],
 
-	rules: {
-		"at-rule-no-unknown": null,
-		"no-invalid-position-at-import-rule": [false],
-		"no-duplicate-selectors": [true, { ignore: ":root" }],
-		"scss/at-rule-no-unknown": [true, { ignoreAtRules: ["use"] }],
-		"plugin/no-unsupported-browser-features": [true, { severity: "error" }],
-	}
+    rules: {
+        "at-rule-no-unknown": null,
+        "no-invalid-position-at-import-rule": null,
+        "selector-pseudo-class-no-unknown": [true, { ignorePseudoClasses: ["global", "local"] }],
+
+        "scss/at-rule-no-unknown": [true, { ignoreAtRules: ["use"] }],
+
+        "plugin/no-unsupported-browser-features": [true, { severity: "error" }],
+    },
 };
