a92091eb453f6d30c14bd5b417a7536ec31c8818
xeho91 <xeho91@pm.me>
2021-07-07 10:18:31 +0800

    build(types): 🚧  Drop the idea of exporting external types

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,9 @@
-export type {
-    ButtonHTMLAttributes,
-    ChangeEvent,
-    Dispatch,
-    FormEvent,
-    FunctionComponent,
-    HTMLProps,
-    MutableRefObject,
-} from "react";
-export type { LinkProps, RouteComponentProps, RouteProps } from "react-router-dom";
-
-export type {
-    ContactActionAdd,
-    ContactActionEdit,
-    ContactActionGet,
-    ContactActionRemove,
-    ContactActionType,
-} from "$reducer";
-
 export interface ContactSchema {
     id: number;
     first_name: string;
     last_name: string;
     job: string;
     description: string;
-}
-
-export interface ContactsState {
-    list: ContactSchema[];
-	deletedIds: number[];
 }
 
 export interface IconifyIcon {
